cc461b23c99f1ff00fbb7aa4686c3f8346cdf824
Pavel Mezencev <mezencev24@mail.ru>
2022-07-02 16:39:35 +0300

    add modal player

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import Slider from './modules/slider';
+import PlayVideo from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
   const slider = new Slider('.page', '.next');
+  const playVideo = new PlayVideo('.showup .play', '.overlay');
+
   slider.render();
+  playVideo.init();
 });
---
+++ src/js/modules/playVideo.js
@@ -0,0 +1,47 @@
+export default class PlayVideo {
+  constructor(triggerBtn, overlay) {
+    this.overlay = document.querySelector(overlay);
+    this.btns = document.querySelectorAll(triggerBtn);
+    this.close = this.overlay.querySelector('.close');
+  }
+
+  bindTrigger() {
+    this.btns.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        if (this.overlay.querySelector('iframe#frame')) {
+          this.overlay.style.display = 'flex';
+        } else {
+          const path = btn.getAttribute('data-url');
+          this.createPlayer(path);
+        }
+      });
+    });
+  }
+
+  bindClose() {
+    this.close.addEventListener('click', () => {
+      console.log(this.close);
+      this.overlay.style.display = 'none';
+      this.player.stopVideo();
+    });
+  }
+
+  createPlayer(path) {
+    this.player = new YT.Player('frame', {
+      height: '100%',
+      width: '100%',
+      videoId: path,
+    });
+    this.overlay.style.display = 'flex';
+  }
+
+  init() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    this.bindTrigger();
+    this.bindClose();
+  }
+}
